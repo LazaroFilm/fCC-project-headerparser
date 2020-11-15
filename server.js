b52da8b7c6642c79fffa7968d9852fcdf7b196cc
LazaroFilm <lazarofilm@gmail.com>
2020-11-15 21:54:52 +0000

    Done

--- server.js
+++ server.js
@@ -2,13 +2,16 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const useragent = require('express-useragent');
+const expressPublicIp = require('express-public-ip');
+
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+const cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,12 +24,28 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
 
+// my code
+app.enable('trust proxy');
+ 
+app.use(expressPublicIp());
+app.use(useragent.express());
+app.set('trust proxy', true)
+
+app.get("/api/whoami", (req, res) => {
+  const ipaddress = req.ip;
+  const language = req.headers["accept-language"];
+  const software = req.useragent.source;
+  res.json({ ipaddress, language, software })
+})
+
+
+// end of my code
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
